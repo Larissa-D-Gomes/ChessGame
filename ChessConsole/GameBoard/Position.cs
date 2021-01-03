5e53a9f67fbd94b66f9256e43c50dbb4c6daea3b
Larissa-D-Gomes <larissadgomes2001@gmail.com>
2021-01-03 15:18:07 -0300

    Criando a classe posição

---
+++ ChessConsole/GameBoard/Position.cs
@@ -0,0 +1,28 @@
+﻿
+namespace GameBoard
+{
+    class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        // Constructor
+
+        // @param int row, int column
+        public Position(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
+        // Methods 
+        public override string ToString()
+        {
+            return this.Row + ", " + this.Column;
+        }
+
+
+
+
+    }
+}

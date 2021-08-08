38c906a34856fbc01f7e7c829ad0489f39bf6711
Erik Shrestha <Eshrestha@student.neumont.edu>
2021-08-07 19:20:10 -0600

    Moved game controller to Controller.cs

--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -1,59 +1,15 @@
 ﻿using System;
 using GameBoard;
 using Chess;
+using ChessConsole.Helpers;
 
 namespace ChessConsole
 {
     class Program
     {
-        // Test three
         static void Main(string[] args)
         {
-            try
-            {
-                ChessMatch cm = new ChessMatch();
-
-                while (!cm.Finished)
-                {
-                    try
-                    {
-                        Console.Clear();
-                        bool[,] possiblePos = null;
-                        View.PrintMatch(cm, possiblePos);
-
-                        Console.Write("\nFrom: ");
-                        Position from = View.ReadChessPosition().ToPosition();
-                        cm.ValidateFromPosition(from);
-
-                        possiblePos = cm.Board.GetPiece(from).GetPossibleMoves();
-
-                        Console.Clear();
-                        View.PrintMatch(cm, possiblePos);
-
-                        Console.Write("\nTo: ");
-                        Position to = View.ReadChessPosition().ToPosition();
-                        cm.ValidateToPosition(from, to);
-                        cm.ExecuteMove(from, to);
-                    }
-                    catch(GameBoardException e)
-                    {
-                        Console.WriteLine("\n" + e.Message + "\nPress enter to continue...");
-                        Console.ReadLine();
-                    }
-                    catch (IndexOutOfRangeException e)
-                    {
-                        Console.WriteLine("\n" + "Invalid Position!" + "\nPress enter to continue...");
-                        Console.ReadLine();
-                    }
-                }
-                Console.Clear();
-                View.PrintMatch(cm, null);
-            }
-            catch(GameBoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            
+            new Controller().Run();
         }
     }
-}
+}  
